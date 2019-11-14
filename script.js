3fb644fa73893ae39689e20fb14c10aa3b6c611b
Taluama Velludo <taluama@gmail.com>
2019-11-14 16:19:27 -0500

    fixed appendchild method

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let itemNumber = 6;
 document.querySelector('.insert-item-box > button').onclick = function(){
   let newLI = document.createElement('li');
   newLI.innerText = `ITEM ${itemNumber++}`;
-  document.querySelector('.insert-item-box > ul').append(newLI);
+  document.querySelector('.insert-item-box > ul').appendChild(newLI); // for some reason .append also works here, even though it's a jQuery method
+
   // console.log('it worked!')
 }
